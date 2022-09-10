7884234ebd12b3709bd50c98b443f50a3b1449ea
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-09-10 22:08:23 +0300

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,8 @@
+function editProfile(){
+ document.getElementById("profile").style.display = "none";
+ document.getElementById("edit-profile").style.display = "block";
+}
+function editThisCode(){
+ document.getElementById("btn").style.display = "none";
+ document.getElementById("secret").style.display = "block";
+}
